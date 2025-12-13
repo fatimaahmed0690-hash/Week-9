19230584be5374d8c72f92d09ffff9f3975cced5
fatimaahmed0690-hash <fatimaahmed0690@gmail.com>
2025-12-13 16:18:42 +0530

    Refactor auth functions and add login_user
    Refactor user authentication functions to simplify registration and add login functionality.

--- auth.py
+++ auth.py
@@ -1,22 +1,16 @@
-import bcrypt
 from database_manager import DatabaseManager
 
 db = DatabaseManager()
 
-def hash_password(password):
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
-
-def verify_password(password, password_hash):
-    return bcrypt.checkpw(password.encode('utf-8'), password_hash.encode('utf-8'))
-
-def register_user(username, password, role="analyst"):
+def register_user(username, password):
+    if not username or not password:
+        return False
     if db.get_user(username):
         return False
-    db.add_user(username, hash_password(password), role)
-    return True
+    return db.add_user(username, password)
 
-def verify_user(username, password):
+def login_user(username, password):
     user = db.get_user(username)
-    if not user:
-        return False
-    return verify_password(password, user[2])  # password_hash is second column
+    if user and user["password"] == password:
+        return True
+    return False
